00ccc8313b4ec2b982f82e2ba8864ab76dab8d9a
luliajuli <yu.poteshkina@gmail.com>
2024-05-11 01:16:07 +0300

    Создаем цикл для обработки попыток угадывания числа

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -9,6 +9,25 @@ public class GuessingGame {
         int attempts = 3;
         // Загадываем число
         secretNumber = random.nextInt(10); // Генерируем случайное число от 0 до 9
+
+        // Цикл для обработки попыток угадывания числа
+        while (attempts > 0) {
+            System.out.println("У вас осталось " + attempts + " попыток.");
+            System.out.print("Введите ваше предположение: ");
+            int guess = scanner.nextInt();
+
+            // Проверяем, угадал ли пользователь число
+            if (guess == secretNumber) {
+                System.out.println("Поздравляем! Вы угадали число.");
+                break; // Выходим из цикла, так как число угадано
+            } else if (guess < secretNumber) {
+                System.out.println("Загаданное число больше вашего предположения.");
+            } else {
+                System.out.println("Загаданное число меньше вашего предположения.");
+            }
+
+            attempts--; // Уменьшаем количество оставшихся попыток
+        }
     }
 }
 
