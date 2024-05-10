aeeeb2d596139c612f7d608daadd9d9426195d72
luliajuli <yu.poteshkina@gmail.com>
2024-05-11 01:15:01 +0300

    В методе main создаем объекты Random и Scanner, инициализируем переменные для хранения загаданного числа и кол-ва попыток

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -3,6 +3,12 @@ import java.util.Scanner;
 
 public class GuessingGame {
     public static void main(String[] args) {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int secretNumber;
+        int attempts = 3;
+        // Загадываем число
+        secretNumber = random.nextInt(10); // Генерируем случайное число от 0 до 9
     }
 }
 
